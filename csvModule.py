f9d17052f8ebbd55baf332ad91f183d1aaabd508
kmchord9 <kmchord9@gmail.com>
2020-12-16 17:08:35 +0900

    loadTodayCSVに取得する最大要素数を設定できるように変更

--- csvModule.py
+++ csvModule.py
@@ -4,30 +4,38 @@ import csv
 import datetime
 
 def saveCSV(time, data, save_path="./datalog/"):
+  now_str = datetime.datetime.now().strftime("%Y%m%d")
+  filename = save_path + now_str + ".csv"
+  with open(filename, 'a') as f:
+    strTime = time.strftime("%Y/%m/%d %H:%M:%S")
+    addStr = strTime + "\t" + str(data)
+    
+    print(addStr, file=f)
+
+def loadTodayCSV(maxelementN, getdate="today"):
+  save_path = "./datalog/"
+  if not (getdate=="today"):
     now_str = datetime.datetime.now().strftime("%Y%m%d")
     filename = save_path + now_str + ".csv"
-    with open(filename, 'a') as f:
-        strTime = time.strftime("%Y/%m/%d %H:%M:%S")
-        addStr = strTime + "\t" + str(data)
-        
-        print(addStr, file=f)
-
-def loadTodayCSV():
-    save_path = "./datalog/"
-    now_str = datetime.datetime.now().strftime("%Y%m%d")
+  else:
+    now_str = getdate
     filename = save_path + now_str + ".csv"
 
-    csv_file = open(filename, 'r')
+  csv_file = open(filename, 'r')
 
-    xdata = []
-    ydata = []
-    reVal = []
+  xdata = []
+  ydata = []
+  reVal = []
 
-    for row in csv.reader(csv_file, delimiter='\t'):
-        xdata.append(row[0])
-        ydata.append(row[1])
+  for row in csv.reader(csv_file, delimiter='\t'):
+    xdata.append(row[0])
+    ydata.append(row[1])
         
+  if xdata < maxelementN: 
     reVal.append([datetime.datetime.strptime(data, '%Y/%m/%d %H:%M:%S') for data in xdata])
     reVal.append([float(data) for data in ydata])
+  else:
+    reVal.append([datetime.datetime.strptime(data, '%y/%m/%d %h:%m:%s') for data in xdata][:maxelementN])
+    reVal.append([float(data) for data in ydata][:maxelementN])
 
-    return reVal
+  return reVal
