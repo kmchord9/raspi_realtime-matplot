2ee3116e815eee229056ad6764492cfff0cacf3b
kazune matsubara <matsubara.kazune@nmiri.city.nagoya.jp>
2021-07-14 09:48:19 +0900

    コメント修正

--- csvModule.py
+++ csvModule.py
@@ -28,36 +28,34 @@ def loadTodayCSV(maxelementN, getdate="today", save_path="./datalog/"):
   '''
     今日の日付でcsvファイルを作成して時間とデータを書き込む
     引数：
-      time        datetime型
-      data        float型
-      save_path   str型
-    save_pathで保存するディレクトリを指定する。
+      maxelementN int型   最新のデータから何個取得するか指定
+      getdate             デフォルトは"today"だが別日の場合にはgetdate="20210730"のように指定する
+      save_path   str型   読み込むファイルのパスを指定する
+    
   '''
-  #save_path = "./datalog/"
+  #読み込むファイルが今日の場合
   if (getdate=="today"):
     now_str = datetime.datetime.now().strftime("%Y%m%d")
     filename = save_path + now_str + ".csv"
+  #読み込むファイルが別の日の場合
   else:
     now_str = getdate
     filename = save_path + now_str + ".csv"
-
-  #csv_file = open(filename, 'r')
-
+  
+  #ファイルを読み込む
   with open(filename, 'r') as csv_file:
     xdata = []
     ydata = []
-
+    #読み込んだファイルを配列に代入する
     for row in csv.reader(csv_file, delimiter='\t'):
-      xdata.append(row[0])
-      ydata.append(row[1])
-
+      xdata.append(row[0])#日付データ
+      ydata.append(row[1])#数値データ
+  
+  #文字列データを変換する
   xdata_str = [datetime.datetime.strptime(data, '%Y/%m/%d %H:%M:%S') for data in xdata]
   ydata_str = [float(data) for data in ydata]
 
-  #if len(xdata) < maxelementN:
-  #    return [xdata_str, ydata_str]
-
-  #else:
+  #maxelementNより保存データ数が少ない場合には全てのデータを取得する
   rexdata_str = xdata_str[max(0,len(xdata_str)-maxelementN):]
   reydata_str = ydata_str[max(0,len(ydata_str)-maxelementN):]
   return [rexdata_str, reydata_str]
